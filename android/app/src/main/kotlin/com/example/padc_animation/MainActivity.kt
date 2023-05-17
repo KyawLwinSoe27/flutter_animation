6910da4349c2ed87321de32cfaf716f01382bee2
Kyaw Lwin Soe <kyawlwinsoe@globalwave.company>
2023-05-17 15:44:15 +0630

    first commit

---
+++ android/app/src/main/kotlin/com/example/padc_animation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.padc_animation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/implicit_animations/explicit_animation.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ImplicitAnimations extends StatelessWidget {
+  const ImplicitAnimations({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
